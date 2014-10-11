b0dc36df6afca16f07421358459e923d0ad0b989
Bilal <bilal.arslan1993@hotmail.com>
2014-10-11 16:02:53 +0300

    Stack Class Added

--- Stack/main.swift
+++ Stack/main.swift
@@ -8,5 +8,21 @@
 
 import Foundation
 
-println("Hello, World!")
+var stack = Stack<Int>()
+
+stack.push(7)
+stack.push(10)
+stack.push(8)
+
+
+stack.printStack()
+
+println("Peek is    : \(stack.peek())")
+println("Count is   : \(stack.count())\n")
+
+println("Poping item: \(stack.pop())\n")
+
+stack.printStack()
+println("Peek is    : \(stack.peek())")
+println("Count is   : \(stack.count())")
 
